01bc001dfd9e21e02ce8f8bb8d2651dce6499122
thisismrsanjay <thisismrsanjay@gmail.com>
2018-05-24 09:23:29 +0530

    try

--- Scrape.js
+++ Scrape.js
@@ -3,6 +3,16 @@ var results = $('li.result-row');
 
 results.each((index,element)=>{
     const result = $(element);
-    const title= result.find('div.swipe-wrap')
-    console.log(title);
+    const imageData = result.find('a.result-image').attr('data-ids');
+    if(imageData){
+        const parts = imageData.split(',');
+        let images = []
+        const ids  = parts.map((id)=>{
+            let url = `https://images.craigslist.org/${id.split(':')[1]}_300x300.jpg`
+            images.push(url);
+        })
+        return images;
+    }
+
+    
 })
--- index.js
+++ index.js
@@ -17,13 +17,28 @@ function getResults(body) {
     const $ = cheerio.load(body);
     const rows = $('li.result-row');
     const results=[];
+    
     rows.each((index, element) => {
         const result = $(element);
         const title = result.find('.result-title').text();
         const price = $(result.find('.result-price').get(0)).text();
+        const imageData = result.find('a.result-image').attr('data-ids');
+         const images = []
+         if(imageData){
+            const parts = imageData.split(',');
+            
+            const ids  = parts.map((id)=>{
+                let url = `https://images.craigslist.org/${id.split(':')[1]}_300x300.jpg`
+                images.push(url);
+            })
+        }
+        const hood = result.find('.result-hood').text().trim().slice(1,-1);
+    
         results.push({
             title,
-            price
+            price,
+            images,
+            hood
         })
     })
     return results;
